5f4ea21eeee23f760df2ae2dcbb46c5705d69e0d
VladKorchik <80666885+VladKorchik@users.noreply.github.com>
2021-08-25 13:47:21 +0300

    Error corrections

---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,57 @@
+package ru.netology.servlet;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+
+    @Override
+    public void init() {
+        AnnotationConfigApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext("ru.netology");
+        controller = applicationContext.getBean(PostController.class);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        final var path = req.getRequestURI();
+        if (path.equals("/api/posts")) {
+            controller.all(resp);
+            return;
+        }
+        if (path.matches("/api/posts/\\d+")) {
+            // easy way
+            final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+            controller.getById(id, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        final var path = req.getRequestURI();
+        if (path.equals("/api/posts")) {
+            controller.save(req.getReader(), resp);
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        final var path = req.getRequestURI();
+        if (path.matches("/api/posts/\\d+")) {
+            // easy way
+            final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
+            controller.removeById(id, resp);
+            return;
+        }
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+}
